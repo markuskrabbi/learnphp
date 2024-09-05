d02e4af45082ec302031e76b43a471326da00fbc
Markus Krabbi <markuskrabbi@gmail.com>
2024-09-05 10:31:36 +0300

    functions

--- index.php
+++ index.php
@@ -1,51 +1,14 @@
 <?php
 
-$a = 1;
-$a++;
-$a--;
-$a+=2;
-$a-=2;
-$a/=2;
-$a*=2;
-$a%=2;
-
-$a = 'hello ' . 'world';
-$a .='askfh';
-$array = [1,2,3];
-$array = array(1,2,3);
-
-var_dump($array[1]);
-$array = [
-    'essa' => 1,
-    'tessa' => 2,
-    'kossa' => 3,
-    5 => 4,
-    'asfaf',
-    10 => 1.2,
-    true
-
-];
-var_dump($array['kossa']);
-
+$array = [1,2,3,4];
+array_push($array,5,6,7);
+$array[] = 8;
+$array[4] = 123123;
 var_dump($array);
-//foreach($array as $key => $value){
-//   echo $value . "\n";
-// }
 
-var_dump($array);
-foreach($array as $key => $value){
-    //echo $key . '=>'. $value . "\n";
-    echo "$key=>$value\n";
-}
-$name ='Markus';
-
-$string = "hello $name\n";
-var_dump($string);
-
-$char = 'a';
-
-for($i=0;$i<26;$i++){
-    var_dump($char);
-    $char++;
+function hello($name=''){
+    echo "hello $name\n";
 }
 
+hello('markus');
+hello();
