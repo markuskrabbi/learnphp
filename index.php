7eb3d399721090b9fb86d8c0361673e508fa868e
Markus Krabbi <markuskrabbi@gmail.com>
2024-09-05 10:04:51 +0300

    Simple types

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+$a = 1;
+$a++;
+$a--;
+$a+=2;
+$a-=2;
+$a/=2;
+$a*=2;
+$a%=2;
+
+$a = 'hello ' . 'world';
+$a .='askfh';
+$array = [1,2,3];
+$array = array(1,2,3);
+
+var_dump($array[1]);
+$array = [
+    'essa' => 1,
+    'tessa' => 2,
+    'kossa' => 3,
+    5 => 4,
+    'asfaf',
+    10 => 1.2,
+    true
+
+];
+var_dump($array['kossa']);
+
+var_dump($array);
+//foreach($array as $key => $value){
+//   echo $value . "\a";
+// }
+
+var_dump($array);
+foreach($array as $key => $value){
+    //echo $key . '=>'. $value . "\a";
+    echo "$key=>$value\n";
+}
+$name ='Markus';
+$string = "hello $name\n";
+var_dump($string);
